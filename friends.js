9b3f90f868fdf2413cb40abfb7de20f918fd0859
Erica Hagle <ericahagle.dev@gmail.com>
2023-07-11 10:46:27 -0700

    Add places to visit

--- friends.js
+++ friends.js
@@ -3,3 +3,8 @@ var arrays = ["Jim", "Joe", "Janice", "Jordan", "Jasper"];
 console.log(arrays[2]);
 console.log(arrays[4]);
 console.log(arrays[0]);
+
+var places = ["Alaska", "Mexico", "Italy", "Greece", "Minnesota"];
+
+places.push("Barbados");
+console.log(places);
